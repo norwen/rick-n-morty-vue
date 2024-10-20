f5701aca416df652e057d4d637daa99223e0efa3
norwen <kalininadev@gmail.com>
2024-10-20 20:00:19 +0200

    feat: init commit

---
+++ src/components/CharacterImage.vue
@@ -0,0 +1,33 @@
+<script setup>
+import characterEmpty from '@/assets/character-empty.png'
+
+defineProps({
+  url: {
+    type: String,
+    default: characterEmpty,
+  },
+  altText: {
+    type: String,
+    default: 'Rick and Morty character',
+  },
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <div class="character-container">
+    <div v-if="isLoading" class="loader">Loading</div>
+    <img v-else :src="url" :alt="altText" />
+  </div>
+</template>
+
+<style scoped>
+.character-container {
+  width: 14rem;
+  height: 14rem;
+  box-shadow: 0 0 34px 0 rgba(0, 0, 0, 0.0915);
+}
+</style>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const characterId = ref('')
+function handleSearch() {}
+</script>
+
+<template>
+  <form @submit.prevent="handleSearch">
+    <label for="search">Search:</label>
+    <input
+      id="search"
+      type="text"
+      v-model="characterId"
+      placeholder="Search..."
+      aria-label="Search for a Rick and Morty character"
+      inputmode="numeric"
+      pattern="^[1-9]\d*$"
+      required
+    />
+    <button type="submit">Search</button>
+  </form>
+</template>
+
+<style scoped></style>
